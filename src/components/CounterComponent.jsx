a4a9e52daf5f4de673aca815ab8746c07f128a47
Shawaiz <shawaizy2k@gmail.com>
2025-02-12 18:28:38 -0500

    Finshed the like button and counter that goes along with it. It now increments up by one after every click

--- src/components/CounterComponent.jsx
+++ src/components/CounterComponent.jsx
@@ -1,13 +1,24 @@
-function CounterComponent() {
-  const button = 0;
+import { useState } from "react";
 
+function CounterComponent() {
+
+  const [count, setCount] = useState(0);
   
+
+  const handleOnClick = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
   return (
     <div className="container">
       <h1 className="text-center">Counter</h1>
-      <div className="fs-2 text-center">Current Number of Likes is: 0</div>
+      <div className="fs-2 text-center">
+        Current Number of Likes is: {count}
+      </div>
       <div className="text-center">
-        <button className="btn btn-primary btn-sm">Like</button>
+        <button onClick={handleOnClick} className="btn btn-primary btn-sm">
+          Like
+        </button>
       </div>
     </div>
   );
